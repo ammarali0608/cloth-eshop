1a92c94b5b9d21613b3bf8b2cfda2cd670f73600
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2022-12-29 15:53:12 +0500

    83. Setting Up A Homepage With Routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-  import CategoryMenu from "./components/categories-menu/categories-menu.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <CategoryMenu categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;

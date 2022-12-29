294c172e357cfd1eba4cb31100ede54d59880446
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2022-12-29 23:35:35 +0500

    88. Setting Up Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
-const Check = () => {
-  return <h1>Heloo</h1>;
+const Shop = () => {
+  return <h1>Shopping Cart is Empty</h1>;
 };
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Check />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
       </Route>
     </Routes>
   );

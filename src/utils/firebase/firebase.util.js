39ae9dd59972dd75e4ff640e5e2de2613d0ea4d9
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2023-01-03 23:20:50 +0500

    95. Finish Creating User Documents

--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -32,4 +32,15 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userSnapShot = await getDoc(userDocRef);
   console.log(userSnapShot);
   console.log(userSnapShot.exists());
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  return userDocRef;
 };

ad5512fe4df88ea0772b0856376f4b755c82235c
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2023-01-04 10:37:09 +0500

    96. Sign In With Redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,40 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.util";
+import { async } from "@firebase/util";
 
 const SignIn = () => {
+  async function effectFunction() {
+    const response = await getRedirectResult(auth);
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  }
+
+  useEffect(() => {
+    effectFunction();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+  const logGoogleRedirectUser = async () => {
+    await signInWithGoogleRedirect();
+  };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign In with Google Redirect
+      </button>
     </div>
   );
 };

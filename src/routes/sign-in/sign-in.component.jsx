b7079650f153665f0ae08053f6f9f72d5a1298b9
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2023-01-04 12:17:46 +0500

    97. Sign Up Form Pt.1

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -6,35 +6,37 @@ import {
   createUserDocumentFromAuth,
   signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.util";
-import { async } from "@firebase/util";
+// import { async } from "@firebase/util";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 const SignIn = () => {
-  async function effectFunction() {
-    const response = await getRedirectResult(auth);
-    if (response) {
-      const userDocRef = await createUserDocumentFromAuth(response.user);
-    }
-  }
+  // async function effectFunction() {
+  //   const response = await getRedirectResult(auth);
+  //   if (response) {
+  //     const userDocRef = await createUserDocumentFromAuth(response.user);
+  //   }
+  // }
 
-  useEffect(() => {
-    effectFunction();
-  }, []);
+  // useEffect(() => {
+  //   effectFunction();
+  // }, []);
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
-  const logGoogleRedirectUser = async () => {
-    await signInWithGoogleRedirect();
-  };
+  // const logGoogleRedirectUser = async () => {
+  //   await signInWithGoogleRedirect();
+  // };
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
+      <SignUpForm />
+      {/* <button onClick={signInWithGoogleRedirect}>
         Sign In with Google Redirect
-      </button>
+      </button> */}
     </div>
   );
 };

e6a3c64de7b261de58261cb8ca04eadde86c24a6
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2023-01-07 21:36:47 +0500

    112. New Shop Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
 
 import Authentication from "./routes/authentication/authentication.component";
-const Shop = () => {
-  return <h1>Shopping Cart is Empty</h1>;
-};
 
 function App() {
   return (

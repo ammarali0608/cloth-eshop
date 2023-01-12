a86e92ac36efaa9417f3ae4c095f65c22cb44a9b
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2023-01-13 00:00:38 +0500

    133. Nested Routes in Shop

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,20 @@
+import { Fragment, useContext } from "react";
+import "./categories-preview.styles.scss";
+import { CategoriesContext } from "../../contexts/categories/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default CategoriesPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,12 @@
-import { Fragment, useContext } from "react";
-import { CategoriesContext } from "../../contexts/categories/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 import "./shop.styles.scss";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="shop-container ">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 

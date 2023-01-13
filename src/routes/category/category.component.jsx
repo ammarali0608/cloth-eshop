1b62350731b688d6d17da038293028a914b7a941
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2023-01-13 18:55:24 +0500

    134. Category Page

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,24 @@
+import { useContext, useState, useEffect } from "react";
+import { CategoriesContext } from "../../contexts/categories/categories.context";
+import { useParams } from "react-router-dom";
+import "./category.styles.scss";
+import ProductCard from "../../components/product-card/product-card.component";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+  return (
+    <div className="category-container">
+      {products &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
+  );
+};
+
+export default Category;
